06fc729a79ec943ad429fd5a3e399d0fd85b2e95
CalebAlfredS <85428170+CalebAlfredS@users.noreply.github.com>
2022-01-08 15:07:17 +0530

    Add files via upload

---
+++ js/SlingShot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(7);
+            stroke(64,224,208);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            
+        }
+    
+           // Write line() to draw a line from pointA to pointB
+
+        }
+    }
+    
